cdd5249a0595a5149d9e6697f502be0cc061e99f
Clay Diffrient <cdiffrient@instructure.com>
2015-10-05 22:12:04 -0600

    WIP: Add quiet times feature
    This allows a quiet time to be specified which will give users
    that send messages during that time period without any other
    activity a message saying that they should remain connected
    and check back later for a response.

---
+++ test/quietTimes.test.js
@@ -0,0 +1,11 @@
+/*eslint-env mocha */
+var expect = require('chai').expect;
+var quietTimes = require('../lib/quietTimes');
+
+describe('Quiet Times', function () {
+  describe('isInQuietTimes', function () {
+    xit('reflects proper quiet times based on the config', function () {
+
+    });
+  });
+});
